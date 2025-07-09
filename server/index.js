815d1526e61dd26015aa1c4fd920b64b8261db9f
raisrihari <sriharirai77@gmail.com>
2025-07-09 00:47:50 -0400

    new commit 2

--- server/index.js
+++ server/index.js
@@ -14,18 +14,19 @@ app.use(cors()); // Enable CORS for all routes
 app.use(express.json({ extended: false }));
 
 // --- API ROUTES ---
-//app.use('/api/users', require('./routes/users'));
-//app.use('/api/dilemmas', require('./routes/dilemmas'));
-//app.use('/api/perspectives', require('./routes/perspectives'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/dilemmas', require('./routes/dilemmas'));
+app.use('/api/perspectives', require('./routes/perspectives'));
 
 // --- Serve static assets in production ---
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-    });
+    // Serve the React app for any route not handled by the API
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    // });
 }
 
 const PORT = process.env.PORT || 5000;
