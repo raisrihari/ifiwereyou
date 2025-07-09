78d33befc59a86a0d12d428f8c4e101177738ad8
raisrihari <sriharirai77@gmail.com>
2025-07-09 16:53:32 -0400

    Fix: removed splat and added any

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     // Serve the React app for any route not handled by the API
-    app.get('/{*splat}', (req, res) => {
+    app.get('/{*any}', (req, res) => {
        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     });
 }
